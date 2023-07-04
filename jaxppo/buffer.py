0fed552b8e47680792c29519e246d3cf6a9a93f8
Yann Berthelot <yannberthelot1@gmail.com>
2023-07-04 16:45:31 +0200

    init buffer + working gae

---
+++ jaxppo/buffer.py
@@ -0,0 +1,86 @@
+import jax.numpy as jnp
+from flax.struct import dataclass
+from typing import Tuple
+import pdb
+
+
+@dataclass
+class Buffer:
+    obs: jnp.array
+    actions: jnp.array
+    logprobs: jnp.array
+    dones: jnp.array
+    values: jnp.array
+    advantages: jnp.array
+    returns: jnp.array
+    rewards: jnp.array
+
+
+def init_buffer(
+    num_steps: int,
+    num_envs: int,
+    observation_space_shape: Tuple[int],
+    action_space_shape: Tuple[int],
+) -> Buffer:
+    """Init the buffer with zero arrays of the right dimension for all values to track"""
+    return Buffer(
+        obs=jnp.zeros((num_steps, num_envs) + observation_space_shape),
+        actions=jnp.zeros((num_steps, num_envs) + action_space_shape),
+        logprobs=jnp.zeros((num_steps, num_envs)),
+        dones=jnp.zeros((num_steps, num_envs)),
+        values=jnp.zeros((num_steps, num_envs)),
+        advantages=jnp.zeros((num_steps, num_envs)),
+        returns=jnp.zeros((num_steps, num_envs)),
+        rewards=jnp.zeros((num_steps, num_envs)),
+    )
+
+
+def insert_buffer(buffer: Buffer, step: int, feature_dict: dict) -> Buffer:
+    """
+    Insert new values into the buffer at the given step.
+    Values are to be fed using a dict following this template:
+        {value_name: new_value}
+        e.g.
+        {"obs":[7.3,1.5], "dones":[0.,1.]}
+    """
+    replace_dict = {
+        key: buffer.__dict__[key].at[step].set(value)
+        for key, value in feature_dict.items()
+    }
+    return buffer.replace(**replace_dict)
+
+
+def update_gae_advantages(
+    buffer: Buffer, next_done: int, next_value: float, gamma: float, gae_lambda: float
+) -> Buffer:
+    """
+    Updates GAE advantages in the buffer using :
+    - the content of the buffer
+    - the next done flag (0 or 1)
+    - the value of the next_state.
+    - gamma (0<=gamma<=1): the discount factor
+    - gae_lambda (0<=gae_lambda<=1) : the gae hyperparameter between bias and variance
+    """
+    # Reset advantages values
+    buffer = buffer.replace(advantages=buffer.advantages.at[:].set(0.0))
+    # Compute advantage using generalized advantage estimate
+    lastgaelam = 0
+    num_steps = buffer.dones.shape[0]
+    for t in reversed(range(num_steps)):
+        if t == num_steps - 1:
+            nextnonterminal = 1.0 - next_done
+            nextvalues = next_value
+        else:
+            nextnonterminal = 1.0 - buffer.dones[t + 1]
+            nextvalues = buffer.values[t + 1]
+        delta = (
+            buffer.rewards[t] + gamma * nextvalues * nextnonterminal - buffer.values[t]
+        )
+        lastgaelam = delta + gamma * gae_lambda * nextnonterminal * lastgaelam
+        buffer = buffer.replace(advantages=buffer.advantages.at[t].set(lastgaelam))
+    return buffer
+
+
+def update_returns(buffer: Buffer) -> Buffer:
+    """Save returns as advantages + values"""
+    return buffer.replace(returns=buffer.advantages + buffer.values)
---
+++ tests/test_buffer.py
@@ -0,0 +1,96 @@
+import jax.numpy as jnp
+from jaxppo.buffer import init_buffer, insert_buffer, update_gae_advantages
+import gymnasium as gym
+import pdb
+import pytest
+
+
+def get_env_action_shape(env: gym.Env):
+    return (
+        (env.action_space.n,)
+        if isinstance(env.action_space, gym.spaces.Discrete)
+        else env.action_space.shape
+    )
+
+
+@pytest.fixture
+def setup_buffer():
+    n_steps = 4
+    n_envs = 2
+    env = gym.make("CartPole-v1")
+    action_space_shape = get_env_action_shape(env)
+    observation_space_shape = env.observation_space.shape
+    return init_buffer(
+        num_steps=n_steps,
+        num_envs=n_envs,
+        action_space_shape=action_space_shape,
+        observation_space_shape=observation_space_shape,
+    )
+
+
+def test_init_buffer(setup_buffer):
+    buffer = setup_buffer
+    n_steps = 4
+    n_envs = 2
+    env = gym.make("CartPole-v1")
+    action_space_shape = get_env_action_shape(env)
+    observation_space_shape = env.observation_space.shape
+
+    assert buffer.dones.shape == (n_steps, n_envs)
+    assert buffer.actions.shape == (n_steps, n_envs, *action_space_shape)
+    assert buffer.obs.shape == (n_steps, n_envs, *observation_space_shape)
+    assert jnp.array_equal(buffer.dones, jnp.zeros(buffer.dones.shape))
+
+
+def test_insert_buffer(setup_buffer):
+    buffer = setup_buffer
+    step = 1
+    feature_dict = {"rewards": jnp.ones(2), "advantages": jnp.ones(2) * 2}
+    buffer = insert_buffer(buffer, step, feature_dict)
+    assert jnp.allclose(buffer.rewards[1], jnp.ones(2))
+    assert jnp.allclose(buffer.advantages[1], jnp.ones(2) * 2)
+
+
+def test_compute_gae():
+    dones = [0, 0, 0, 0]
+    values = [0.1, 0.1, 0.1, 0.1]
+    rewards = [1.0, 1.0, 1.0, 1.0]
+    next_value = 0.5
+    next_done = 1
+    gamma = 0.9
+    gae_lambda = 0.9
+
+    # Compute "flat" advantages
+    advantage_4 = rewards[-1] + gamma * next_value * (1 - next_done) - values[-1]
+    advantage_3 = rewards[-2] + gamma * values[-1] * (1 - dones[-1]) - values[-2]
+    advantage_2 = rewards[-3] + gamma * values[-2] * (1 - dones[-2]) - values[-3]
+    advantage_1 = rewards[-4] + gamma * values[-3] * (1 - dones[-3]) - values[-4]
+
+    # Compute GAE/exponential average
+    gae_4 = advantage_4
+    gae_3 = advantage_3 + gae_lambda * gamma * gae_4
+    gae_2 = advantage_2 + gae_lambda * gamma * gae_3
+    gae_1 = advantage_1 + gae_lambda * gamma * gae_2
+
+    n_steps = 4
+    n_envs = 1
+    env = gym.make("CartPole-v1")
+    action_space_shape = get_env_action_shape(env)
+    observation_space_shape = env.observation_space.shape
+
+    buffer = init_buffer(
+        num_steps=n_steps,
+        num_envs=n_envs,
+        action_space_shape=action_space_shape,
+        observation_space_shape=observation_space_shape,
+    )
+    for step in range(4):
+        buffer = insert_buffer(
+            buffer,
+            step,
+            {"dones": dones[step], "rewards": rewards[step], "values": values[step]},
+        )
+    buffer = update_gae_advantages(buffer, next_done, next_value, gamma, gae_lambda)
+    assert jnp.allclose(
+        buffer.advantages, jnp.array([[gae_1], [gae_2], [gae_3], [gae_4]])
+    )
