34186cb33b7946859e1d9a1a3beb2581c669c1c6
Yann Berthelot <yannberthelot1@gmail.com>
2023-07-15 23:17:31 +0200

    refactored logging

--- jaxppo/wandb_logging.py
+++ jaxppo/wandb_logging.py
@@ -1,6 +1,7 @@
 """Helpers for weights&biases logging"""
 from dataclasses import dataclass
 
+import jax
 import jax.numpy as jnp
 
 import wandb
@@ -17,7 +18,7 @@ class LoggingConfig:
 
 def init_logging(logging_config: LoggingConfig):
     """Init the wandb run with the logging config"""
-    wandb.init(  # type: ignore[attr-defined]
+    wandb.init(
         project=logging_config.project_name,
         name=logging_config.run_name,
         save_code=True,
@@ -31,14 +32,22 @@ def wandb_log(info):
     return_values = info["returned_episode_returns"][info["returned_episode"]]
     timestep = jnp.max(info["timestep"]) * 8
     wandb.log(
-        {"mean_returns_over_batch": jnp.mean(return_values), "timestep": timestep}
+        {"Train/mean_returns_over_batch": jnp.mean(return_values), "timestep": timestep}
     )
 
 
 def log_variables(variables_to_log: dict, commit: bool = False):
     """Log variables (in form of a dict of names and values) into wandb.
     Commit to finish a step."""
-    wandb.log(variables_to_log, commit=commit)  # type: ignore[attr-defined]
+    wandb.log(variables_to_log, commit=commit)
+
+
+def wandb_test_log(rewards: jax.Array):
+    """Log the test results into wandb"""
+    hist = wandb.Histogram(rewards.tolist())
+    wandb.log({"Test/Episodic reward": hist})
+    # log_variables({"Test/Episodic reward": hist})
+    wandb.run.summary["mean_test_reward"] = jnp.mean(rewards)
 
 
 def finish_logging():
