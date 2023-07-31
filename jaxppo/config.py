19bb782d30a2a87005b97d8db1f976f4ac2a7d32
Yann Berthelot <yannberthelot1@gmail.com>
2023-07-31 16:20:28 +0200

    add serialization of non native types

--- jaxppo/config.py
+++ jaxppo/config.py
@@ -1,10 +1,10 @@
 """Config for PPO agent"""
 import json
-from typing import Any, Optional, Sequence
+from typing import Any, NoReturn, Optional, Sequence
 
 from gymnax.environments.environment import Environment
 from gymnax.wrappers.purerl import GymnaxWrapper
-from pydantic import BaseModel, ConfigDict, field_validator
+from pydantic import BaseModel, ConfigDict, field_serializer, field_validator
 
 from jaxppo.wandb_logging import LoggingConfig
 
@@ -33,7 +33,9 @@ class PPOConfig(BaseModel):
 
     @field_validator("env_id")
     @classmethod
-    def check_env_id_and_params(cls, env_id, info):
+    def check_env_id_and_params(
+        cls, env_id: str | Environment | GymnaxWrapper, info: Any
+    ) -> (str | Environment | GymnaxWrapper) | NoReturn:
         """Check that value environment is a valid gymnax env id, gymnax env, or gymnax wrapped env"""
 
         env_params = info.data["env_params"]
@@ -55,6 +57,15 @@ class PPOConfig(BaseModel):
                 " environment or a gymnax wrapped_env"
             )
 
-    def to_dict(self):
+    @field_serializer("env_params", "env_id")
+    def serialize_env_or_env_params(
+        self, value: str | Environment | GymnaxWrapper
+    ) -> Optional[str]:
+        """Serialize normally un-hashable types by using str representation"""
+        if isinstance(value, str) or value is None:
+            return value
+        return str(value.__class__)
+
+    def to_dict(self) -> dict:
         """Converts the Pydantic config to a dict"""
         return json.loads(self.model_dump_json())
