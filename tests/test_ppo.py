9103e2d9fd59d6e774dd6f118cd6f193e551d8f8
Yann Berthelot <yannberthelot1@gmail.com>
2023-08-05 13:02:57 +0200

    global refactoring + shared networks

--- tests/test_ppo.py
+++ tests/test_ppo.py
@@ -1,12 +1,58 @@
 """Test ppo"""
+
 import gymnasium as gym
 import gymnax
+import jax.numpy as jnp
+import numpy as np
 import pytest
 import wandb
 from gymnax.wrappers.purerl import FlattenObservationWrapper  # pylint: disable=C0411
 
-from jaxppo.ppo_pure_rl import PPO, make_train
+from jaxppo.ppo import PPO
+from jaxppo.train import Transition, _calculate_gae, make_train
 from jaxppo.wandb_logging import LoggingConfig
+
+
+def test_compute_gae():
+    """Check that the computation of gae matches with sb3"""
+    dones = [False, False, True]
+    values = [1.0, 1.0, 1.0]
+    rewards = [1.0, 1.0, 1.0]
+    traj_batch = Transition(
+        done=jnp.array(dones),
+        action=jnp.array(dones),
+        value=jnp.array(values),
+        reward=jnp.array(rewards),
+        log_prob=jnp.array(dones),
+        obs=jnp.array(dones),
+        info=jnp.array(dones),
+    )
+    dones = [False, False, False]
+    last_val = jnp.array(0.5)
+    last_done = True
+    gamma = 0.5
+    gae_lambda = 0.95
+    advantages, targets = _calculate_gae(
+        traj_batch, last_val, gamma=gamma, gae_lambda=gae_lambda
+    )
+    ##### from sb3 #####
+    last_gae_lam = 0
+    buffer_size = 3
+    exepected_advantages = np.zeros(buffer_size)
+    for step in reversed(range(buffer_size)):
+        if step == buffer_size - 1:
+            next_non_terminal = 1.0 - last_done
+            next_values = last_val
+        else:
+            next_non_terminal = 1.0 - dones[step + 1]
+            next_values = values[step + 1]
+        delta = rewards[step] + gamma * next_values * next_non_terminal - values[step]
+        last_gae_lam = delta + gamma * gae_lambda * next_non_terminal * last_gae_lam
+        exepected_advantages[step] = last_gae_lam
+    expected_targets = exepected_advantages + values
+    ##### from sb3 #####
+    assert jnp.allclose(advantages, exepected_advantages)
+    assert jnp.allclose(targets, expected_targets)
 
 
 def test_trained_ppo_pre_defined_env():
@@ -107,9 +153,9 @@ def test_ppo_test_fails_without_agent_state():
 
 def test_ppo_train_and_test_shared_network():
     """Test that the ppo train function doesn't fail"""
-    num_envs = 4
-    total_timesteps = int(1e2)
-    num_steps = 8
+    num_envs = 2
+    num_steps = 4
+    total_timesteps = int(num_envs * num_steps * 2)
     learning_rate = 2.5e-4
     env_id = "CartPole-v1"
     agent = PPO(
@@ -127,9 +173,9 @@ def test_ppo_train_and_test_shared_network():
 
 def test_ppo_train_and_test():
     """Test that the ppo train function doesn't fail"""
-    num_envs = 4
-    total_timesteps = int(1e2)
-    num_steps = 8
+    num_envs = 2
+    num_steps = 4
+    total_timesteps = int(num_envs * num_steps * 2)
     learning_rate = 2.5e-4
     env_id = "CartPole-v1"
     agent = PPO(
@@ -151,9 +197,9 @@ def test_ppo_train_and_log():
     num_steps = 8
     learning_rate = 2.5e-4
     env_id = "CartPole-v1"
-    wandb.init(mode="disabled")
+
     try:
-        logging_config = LoggingConfig("Test", "test", {"test": 1})
+        logging_config = LoggingConfig("Test", "test", {"test": 1}, mode="disabled")
         agent = PPO(
             total_timesteps=total_timesteps,
             num_steps=num_steps,
