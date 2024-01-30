fixed continuous actions broadcasting

jaxppo/networks/networks_classic.py
@@ -142,6 +142,7 @@ class Network(nn.Module):
 
 def init_networks(
     env: Environment,
+    params: EnvParams,
     actor_architecture: Sequence[Union[str, ActivationFunction]],
     critic_architecture: Optional[Sequence[Union[str, ActivationFunction]]] = None,
     multiple_envs: bool = True,
@@ -149,7 +150,7 @@ def init_networks(
 ) -> Tuple[Network, Network]:
     """Create actor and critic adapted to the environment and following the\
           given architectures"""
-    num_actions = get_num_actions(env)
+    num_actions = get_num_actions(env, params)
     actor = Network(
         input_architecture=actor_architecture,
         actor=True,

tests/common/test_networks.py
@@ -89,12 +89,12 @@ def test_critic_init(setup_simple_critic):
 
 
 def test_network_init():
-    env, _ = gymnax.make("CartPole-v1")
+    env, params = gymnax.make("CartPole-v1")
     num_actions = 2
     actor_architecture = ["32", "tanh", "32", "tanh"]
     critic_architecture = ["32", "relu", "32", "relu"]
     actor, critic = init_networks(
-        env, actor_architecture, critic_architecture, multiple_envs=False
+        env, params, actor_architecture, critic_architecture, multiple_envs=False
     )
     expected_actor = nn.Sequential([
         nn.Dense(32),
@@ -120,7 +120,7 @@ def test_predict_value():
     actor_architecture = ["32", "tanh", "32", "tanh"]
     critic_architecture = ["32", "relu", "32", "relu"]
     actor, critic = init_networks(
-        env, actor_architecture, critic_architecture, multiple_envs=False
+        env, env_params, actor_architecture, critic_architecture, multiple_envs=False
     )
 
     key = random.PRNGKey(42)
@@ -148,7 +148,7 @@ def test_predict_probs():
     actor_architecture = ["32", "tanh", "32", "tanh"]
     critic_architecture = ["32", "relu", "32", "relu"]
     actor, critic = init_networks(
-        env, actor_architecture, critic_architecture, multiple_envs=False
+        env, env_params, actor_architecture, critic_architecture, multiple_envs=False
     )
 
     key = random.PRNGKey(42)
@@ -169,4 +169,4 @@ def test_predict_probs():
         actor_state=actor_state, actor_params=actor_state.params, obs=obs
     )
     assert isinstance(probs, jax.Array)
-    assert len(probs) == get_num_actions(env)
+    assert len(probs) == get_num_actions(env, env_params)